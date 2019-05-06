801729387629ba008488de91244972d7515aa920
Akshay <upadhyayakshay91197@gmail.com>
2019-05-06 18:21:13 +0530

    first commit

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = 'b870787c5ae789bcd75b';
+    this.client_secret = '3d60e60e6a30e2e54485cec4c654861dfcddd6d3';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
